e6dfb2a846830ff5de12d7141502153b989a3d9c
ssar <ssar@nate.com>
2022-06-09 17:51:06 +0900

    보충 수업 2 - 오버로딩

--- src/site/metacoding/study/ex03/protoss/Dragoon.java
+++ src/site/metacoding/study/ex03/protoss/Dragoon.java
@@ -1,21 +1,21 @@
 package site.metacoding.study.ex03.protoss;
 
-public class Zealot {
+public class Dragoon {
 	public final String name; // 이름
-	private static int power = 10; // 공격력
-	private static int armor = 10; // 방어력
+	private static int power = 15; // 공격력
+	private static int armor = 5; // 방어력
 	private int hp; // 체력
 	
 	public int getPower() {
 		return power;
 	}
 	
-	public void 공격당함(Zealot unit) {
+	public void 공격당함(Dragoon unit) {
 		hp = hp - unit.getPower();
 		System.out.println(this.name+"가 "+unit.name+"에게 공격당했습니다. 현재 hp : "+this.hp);
 	}
 	
-	public void 공격당함(Dragoon unit) {
+	public void 공격당함(Zealot unit) {
 		hp = hp - unit.getPower();
 		System.out.println(this.name+"가 "+unit.name+"에게 공격당했습니다. 현재 hp : "+this.hp);
 	}
@@ -30,7 +30,7 @@ public class Zealot {
 		System.out.println("방어력이 1증가하였습니다.");
 	}
 
-	public Zealot(String name) {
+	public Dragoon(String name) {
 		super();
 		this.name = name;
 		this.hp = 100;
